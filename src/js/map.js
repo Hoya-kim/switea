b72f05c95a3d261092e3871c003fc23f45bf2c42
Hoya-kim <1203play@gmail.com>
2021-11-15 21:19:33 +0900

    feat: Test by Realtime db

--- src/js/map.js
+++ src/js/map.js
@@ -1,3 +1,11 @@
+// kakaoMap.searchByCategory();
+// kakaoMap.searchByKeyword('동판교로 123 푸르지오');
+// kakaoMap.search('강남');
+
+// import { initializeApp } from 'firebase/app';
+// import { initializeApp } from 'firebase/app';
+// import { getDatabase } from 'firebase-admin/database';
+import axios from 'axios';
 import * as kakaoMap from './utils/kakaoMap';
 
 const mainArea = document.getElementById('map');
@@ -8,3 +16,11 @@ kakaoMap.initMapView(mapContainer);
 
 // 현재위치 설정
 kakaoMap.setGeoMarker();
+
+// kakaoMap.setMarkers(sample);
+(async () => {
+  const { data } = await axios.get(
+    `https://switea-19c19-default-rtdb.firebaseio.com/studies.json`,
+  );
+  kakaoMap.setMarkers(data);
+})();
