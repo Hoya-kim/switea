89b9c8e898b84ea8ff1411ef0c4221b8c1bd1f4b
Hoya-kim <1203play@gmail.com>
2021-11-18 02:00:47 +0900

    feat: Add spinner on whole pages and remove logic on map page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import './scss/index.scss';
 import navigation from './js/components/navigation';
+import spinner from './js/components/spinner';
 
 window.customElements.define('nav-component', navigation);
+
+window.addEventListener('DOMContentLoaded', () => {
+  spinner.addOnView(document.querySelector('.container'));
+});
